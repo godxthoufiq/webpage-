c4d4bb21f2bcf6a5af5ae05338c54126485cce01
godxthoufiq <mohammedthoufiq2627@gmail.com>
2026-03-24 11:50:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    // Proper credentials
+    let validEmail = "admin@cbit.com";
+    let validPassword = "12345";
+
+    if (email === "" || password === "") {
+        alert("Please fill all fields!");
+        return;
+    }
+
+    if (email === validEmail && password === validPassword) {
+        alert("Login Successful ✅");
+        window.location.href = "index.html"; // or dashboard page
+    } else {
+        alert("Invalid Email or Password ❌");
+    }
+}
